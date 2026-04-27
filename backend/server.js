d2ae5aa1a0a832e36333690e27599e4cd4caead0
abhilash6102 <kanchukota.abhilash6102@gmail.com>
2026-04-28 00:23:27 +0530

    changed files

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection
+
 mongoose.connect("mongodb+srv://kanchukotaabhilash6102_db_user:EuqM3vI9VFwecWIs@cluster0.psqnilr.mongodb.net/userDB?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
+
 
 // Schema
 const UserSchema = new mongoose.Schema({
@@ -19,29 +20,45 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
-// ✅ CREATE
+// CREATE
 app.post("/addUser", async (req, res) => {
-    const user = new User(req.body);
-    await user.save();
-    res.send("User Added");
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.send("User Added");
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
-// ✅ READ
+// READ
 app.get("/getUsers", async (req, res) => {
-    const users = await User.find();
-    res.json(users);
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
-// ✅ DELETE
+// DELETE
 app.delete("/deleteUser/:id", async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
-    res.send("User Deleted");
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.send("User Deleted");
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
-// ✅ UPDATE
+// UPDATE
 app.put("/updateUser/:id", async (req, res) => {
-    await User.findByIdAndUpdate(req.params.id, req.body);
-    res.send("User Updated");
+    try {
+        await User.findByIdAndUpdate(req.params.id, req.body);
+        res.send("User Updated");
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
 app.listen(5000, () => console.log("Server running on port 5000"));
